363593ea4146fd90733a936cfa4445cb9a8298d5
Thanwa JDR. <thanwa.jdr@gmail.com>
2024-09-21 01:23:59 +0800

    feat: add deploy script for mock usdc

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,18 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
+
+// read private key from .env file
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.27",
+  solidity: '0.8.27',
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    gnosisChiado: {
+      url: 'https://1rpc.io/gnosis',
+      accounts: [process.env.DEPLOYER_PK],
+    },
+  },
 };
